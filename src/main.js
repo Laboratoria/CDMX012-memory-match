ca70fb1b7e7bab47dd78edaa0e847021ef2da316
BetsyRms <aby_7710@hotmail.com>
2022-03-16 19:12:23 -0700

    Muestra data random, se eliminaron links

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import App from './components/App.js';
+import webdev from './data/webdev/webdev.js';
+
+const webdev1 = webdev.items;
+const webdev2 = webdev.items;
+let webdevRandom = webdev1.concat(webdev2);
+webdevRandom = webdevRandom.sort(function(){return Math.random() - 0.5 });
+console.log(webdevRandom);
 
 document.getElementById('root').appendChild(App());
