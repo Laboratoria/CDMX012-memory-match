9bb40c0f5dd317265c0bbca042305aa181045939
BetsyRms <aby_7710@hotmail.com>
2022-03-19 11:33:07 -0700

    Boton jugar funciona, muestra tarjetas en orden aleatorio

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,13 @@
-//
-// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
-// JavasSript que contenga el `export` correspondiente...
-//
-//  import webdev from '../data/webdev/webdev.js';
+//import webdev from '../data/webdev/webdev.js';
 // console.log(webdev);
-//
 
-const App = () => {
-  const el = document.createElement('div');
 
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
 
-  return el;
-};
 
-export default App;
+// const App = () => {
+//   const el = document.createElement('div');
+//   el.className = 'App';
+//   el.textContent = 'Hola mundo!';
+//   return el;
+// };
+// export default webdevCard;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-import App from './components/App.js';
+// import webdevCard from './components/App.js';
 import webdev from './data/webdev/webdev.js';
 
 const webdev1 = webdev.items;
 const webdev2 = webdev.items;
-let webdevRandom = webdev1.concat(webdev2);
-webdevRandom = webdevRandom.sort(function(){return Math.random() - 0.5 });
-console.log(webdevRandom);
+let webdevJoin= webdev1.concat(webdev2);
+let images = []
+webdevJoin.forEach(element => images.push(element.image));
+images.sort(function(){return Math.random() - 0.5 });
 
-document.getElementById('root').appendChild(App());
+const webdevCard = () => {
+  const image = document.createElement('div');
+  image.className = 'cards-conteiner';
+  for(let i=0; i <images.length; i++){
+    image.innerHTML += `<img src="${images[i]}" class="image">`;
+  }
+  console.log(image);
+  return image;
+};
+
+document.getElementById('root').appendChild(webdevCard());
+
+// document.getElementById('root').appendChild(App());
+
