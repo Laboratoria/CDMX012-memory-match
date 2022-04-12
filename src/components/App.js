5e9f2953256193a2bb58c56a629f9e7434e769b0
BetsyRms <aby_7710@hotmail.com>
2022-04-11 18:01:31 -0600

    falta bloquera tercer cliclk

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,63 @@ import pokemon from '../data/pokemon/pokemon.js';
 const pokemon1 = pokemon.items;
 const pokemon2 = pokemon.items;
 let pokemonJoin= pokemon1.concat(pokemon2);
-let images = []
-pokemonJoin.forEach(element => images.push(element.image));
-images.sort(function(){return Math.random() - 0.5 });
+pokemonJoin.sort(function(){return Math.random() - 0.5 });
+
+let previous = null;
+let pokeballClick = null;
+let clicks = [];
 
 export const pokemonCard = () => {
   const cardsConteiner = document.createElement('div');
   cardsConteiner.className = 'cards-conteiner';
-  // for(let i=0; i <images.length; i++){
-  //   cardsConteiner.innerHTML += `<img src="${images[i]}" class="image">`;
-  // }
-  images.forEach(image => {
-    cardsConteiner.innerHTML += `<img src="${image}" class="image">`;
-  })
+  pokemonJoin.forEach(element => {
+    const div = document.createElement('div');
+    div.className ='pokeballs-pokemons';
+    const pokemons = document.createElement('img');
+    pokemons.className ='pokemons';
+    pokemons.src = element.image;
+    const pokeball = document.createElement('img');
+    pokeball.className ='pokeball';
+    pokeball.src = 'pokeball.png';
+    pokeball.id = element.id
+    pokeball.style.visibility = "hidden"
+    pokeball.addEventListener("click", (event) => {
+      clicks.push(event);
+      console.log('clicks',clicks);
+      if(clicks.length<3){
+        if(previous===null){
+        console.log('primer click');
+        previous = element;
+        pokeballClick = pokeball;
+        console.log(pokeballClick);
+        pokeball.style.visibility = "hidden"
+        }else if(previous!=null){
+        console.log('segundo click');
+        pokeball.style.visibility = "hidden"
+        if(previous.id===element.id){
+          console.log('match');
+          pokeballClick.style.visibility = "hidden"
+          pokeball.style.visibility = "hidden"
+          }else if(previous.id!=element.id){
+          console.log('noMatch');
+          console.log({previous});
+          console.log({pokeballClick});
+          setTimeout(()=>{
+            pokeballClick.style.visibility = "visible"; 
+            pokeballClick=null;
+            clicks=0;
+            pokeball.style.visibility = "visible"
+          }, 1500);
+        }
+        previous=null;
+        }
+       console.log(previous);
+      }
+    });
+    div.appendChild(pokemons);
+    div.appendChild(pokeball);
+    cardsConteiner.appendChild(div);
+    window.onload = setTimeout(()=>{pokeball.style.visibility = "visible"}, 1000);
+  });
   return cardsConteiner;
 };
--- src/main.js
+++ src/main.js
@@ -1,12 +1,2 @@
 import { pokemonCard } from './components/App.js';
 document.getElementById('cards').appendChild(pokemonCard());
-
-let pokeballs = document.getElementsByClassName('pokeball');
-console.log(pokeballs);
-
-Array.from(pokeballs).forEach(element=>{
-  element.addEventListener("click", () => {
-    console.log(element);
-    element.style.visibility = "hidden";
-  })
-})
